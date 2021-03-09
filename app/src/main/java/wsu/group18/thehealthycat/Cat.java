e229f51569fa76d0a0a7272b2b929283253da5d2
bmwettle <bmwettle@gmail.com>
2021-03-09 10:31:58 -0500

    testing commit

--- app/src/main/java/wsu/group18/thehealthycat/Cat.java
+++ app/src/main/java/wsu/group18/thehealthycat/Cat.java
@@ -5,28 +5,65 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.os.Build;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.auth.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import com.google.firebase.database.ValueEventListener;
+
 public class Cat implements Serializable {
 
+    public int UID;
     private String Name;
     private double TargetWeightLBS;
     private double CurrentWeightLBS;
-    private List HistoricalWeightData;
-    private List FeedingTimes;
+    private ArrayList<HistoricalWeightEvent> HistoricalWeightData;
+    private ArrayList<LocalTime> FeedingTimes;
+    private static final String TAG = "ReadAndWriteSnippets";
 
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
+    // [END declare_database_ref]
+
+    FirebaseUser User;
     public Cat(){
         Name = "";
         TargetWeightLBS = 0.0;
         CurrentWeightLBS = 0.0;
         HistoricalWeightData = new ArrayList<HistoricalWeightEvent>();
+        HistoricalWeightEvent first= null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            first = new HistoricalWeightEvent(13, LocalDateTime.of(2019,01,1,3,14));
+        }
+        HistoricalWeightData.add(first);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            HistoricalWeightData.add(new HistoricalWeightEvent(16,LocalDateTime.of(2019,01,11,3,14)));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            HistoricalWeightData.add(new HistoricalWeightEvent(19,LocalDateTime.of(2019,01,22,3,14)));
+        }
         FeedingTimes = new ArrayList<LocalTime>();
-    }
 
-    public Cat(String name, double targetWeight, double currentWeight, List<HistoricalWeightEvent> historicalWeightData, List<LocalTime> feedingTimes) {
+    }
+public void setUser(FirebaseUser user){
+        User= user;
+}
+    public Cat(String name, double targetWeight, double currentWeight, ArrayList<HistoricalWeightEvent> historicalWeightData, ArrayList<LocalTime> feedingTimes, FirebaseUser user) {
         Name = name;
         TargetWeightLBS = targetWeight;
         CurrentWeightLBS = currentWeight;
         HistoricalWeightData = historicalWeightData;
         FeedingTimes = feedingTimes;
+        User = user;
     }
 
     public String getName() {
@@ -55,7 +92,7 @@ public class Cat implements Serializable {
         return HistoricalWeightData;
     }
 
-    public void setHistoricalWeightData(List historicalWeightData) {
+    public void setHistoricalWeightData(ArrayList historicalWeightData) {
         HistoricalWeightData = historicalWeightData;
     }
 
@@ -63,9 +100,12 @@ public class Cat implements Serializable {
         return FeedingTimes;
     }
 
-    public void setFeedingTimes(List feedingTimes) {
+    public void setFeedingTimes(ArrayList feedingTimes) {
         FeedingTimes = feedingTimes;
     }
-
+public void updateFirebase(String connection){
+    mDatabase = FirebaseDatabase.getInstance().getReference("usersData");
+    mDatabase.child(connection).setValue(this);
+}
 }
 
